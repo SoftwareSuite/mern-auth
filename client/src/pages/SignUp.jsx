8657473c8f3ecb8717fc63b06a10e2b1e6d18cf0
Usman Ghori <121635866+SoftwareSuite@users.noreply.github.com>
2024-03-19 06:56:58 -0400

    complete sign up page functionality.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,21 +3,35 @@ import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
-  
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const handleChange = (e) =>{
     setFormData({ ...formData, [e.target.id]: e.target.value})
   }
   const handleSubmit = async (e) => {
     e.preventDefault();
+   try {
+    setLoading(true);
+    setError(false);
     const res = await fetch('/api/auth/signup', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(formData),
-});
-  const data = await res.json();
-    console.log(data);
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    const data = await res.json();
+    setLoading(false);
+    if (data.success === false) {
+      setError(true);
+      return;
+    }
+    
+   } catch (error) {
+    setLoading(false);
+    setError(true);
+   };
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -34,15 +48,16 @@ export default function SignUp() {
         <input type='password' placeholder='password' id='password' className='bg-slate-100 rounded-lg p-3'
         onChange={handleChange} />
 
-        <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 uppercase font-medium disabled:opacity-80'>sign up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 uppercase font-medium disabled:opacity-80'
+        >{loading? "Loading" : "Sign Up"}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
         <Link to='/sign-in'>
         <span className='text-blue-500 font-medium'>Sign In</span>
         </Link>
-        
       </div>
+      <p className='text-red-600 mt-5'>{error && "Something went wrong"}</p>
     </div>
   )
 }
